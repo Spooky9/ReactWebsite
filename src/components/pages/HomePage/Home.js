0dce8ecfd2a8c32363549532e11aadc8b60e43bc
Sergio <sergioh182@gmail.com>
2020-10-13 21:16:34 -0700

    Add footer

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,11 +1,16 @@
-import React from "react";
-import HeroSection from "../../HeroSection";
-import { homeObjOne } from "./Data";
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+// import Pricing from '../../Pricing';
 
 function Home() {
   return (
     <div>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjTwo} />
+      {/* <Pricing /> */}
+      <HeroSection {...homeObjFour} />
     </div>
   );
 }
